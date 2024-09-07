947330635645b904e326716647ba3daaa7ce93c4
Morgan Kpassi <>
2024-09-07 21:55:15 +0200

    component footer

--- app.vue
+++ app.vue
@@ -2,11 +2,13 @@
 import ThreeAvatar from '~/components/ThreeAvatar.vue';
 import ProjectCard from './components/ProjectCard.vue';
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 export default {
   components: {
     ThreeAvatar,
     ProjectCard,
-    Header
+    Header,
+    Footer
   },
   data() {
     return {
@@ -66,20 +68,6 @@ export default {
 </script>
 <template>
   <div class="bg-gradient-to-b from-purple-500 to-pink-300 text-white text-base">
-    <!-- <header class="container mx-auto text-xs font-mons py-7">
-      <div class="flex justify-between">
-        <div class="px-1">
-          <img src="/img/logo-white.png" alt="Logo" class="w-20">
-        </div>
-        <nav class="flex gap-5 text-base ">
-          <a href="/" class="mr-4 text-center">Accueil</a>
-          <a href="#a-propos" class="mr-4 text-center">À propos</a>
-          <a href="#project" class="mr-4 text-center">Projets</a>
-          <a href="#contact" class="mr-4 text-center">Contact</a>
-        </nav>
-        <div>Call me. (+33) 06 66 84 23 88</div>
-      </div>
-    </header> -->
     <Header />
     <main>
       <section class="lg:py-16 py-8 container mx-auto font-mons text-center flex flex-col gap-12">
@@ -148,41 +136,6 @@ export default {
         </div>
       </section>
     </main>
-    <footer class="bg-purple-500 py-12 lg:py-20">
-      <div class="container mx-auto flex flex-col justify-center items-center text-center gap-12 lg:gap-0 lg:flex-row lg:justify-between">
-        <div class="flex flex-col gap-2">
-          <h3 class="text-4xl font-right">Morgan KPASSI</h3>
-          <p>Lyon, France</p>
-        </div>
-        <div class="flex flex-col gap-2 lg:gap-4">
-          <div class="font-medium text-lg lg:text-2xl">Menu</div>
-          <div class="flex flex-col gap-1">
-            <a href="/" class="hover:text-white">Accueil</a>
-            <a href="#a-propos" class="hover:text-white">À propos</a>
-            <a href="#project" class="hover:text-white">Projets</a>
-            <a href="#contact" class="hover:text-white">Contact</a>
-          </div>
-        </div>
-        <div class="flex flex-col gap-2 lg:gap-4">
-          <div class="font-medium text-lg lg:text-2xl">Contact</div>
-          <div class="flex flex-col gap-2">
-            <a href="mailto:morgan.kpassi@gmail.com" class="hover:text-white">morgan.kpassi@gmail.com</a>
-            <div>(+33) 6 66 84 23 88</div>
-            <div class="flex justify-center gap-3">
-                <a href="https://codepen.io/kps-243" target="_blank" rel="noopener noreferrer" aria-label="codepen">
-                  <img src="/img/codepen.svg" alt="Icon CodePen" class="h-6">
-                </a>
-                <a href="https://www.linkedin.com/in/morgan-kpassi-855509211/" target="_blank" rel="noopener noreferrer"
-                  aria-label="linkedin">
-                  <img src="/img/linkedin.svg" alt="Icon Linkedin" class="h-6">                
-                </a>
-                <a href="https://github.com/kps-243" target="_blank" rel="noopener noreferrer" aria-label="github">
-                  <img src="/img/github.svg" alt="Icon Github" class="h-6">                
-                </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <Footer/>
   </div>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+<footer class="bg-purple-500 py-12 lg:py-20">
+    <div class="container mx-auto flex flex-col justify-center items-center text-center lg:text-left gap-12 lg:gap-0 lg:flex-row lg:justify-between">
+      <div class="flex flex-col gap-2">
+        <h3 class="text-4xl font-right">Morgan KPASSI</h3>
+        <p>Lyon, France</p>
+      </div>
+      <div class="flex flex-col gap-2 lg:gap-4">
+        <div class="font-medium text-lg lg:text-2xl">Menu</div>
+        <div class="flex flex-col gap-1">
+          <a href="/" class="hover:text-white">Accueil</a>
+          <a href="#a-propos" class="hover:text-white">À propos</a>
+          <a href="#project" class="hover:text-white">Projets</a>
+          <a href="#contact" class="hover:text-white">Contact</a>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 lg:gap-4">
+        <div class="font-medium text-lg lg:text-2xl">Contact</div>
+        <div class="flex flex-col gap-2">
+          <a href="mailto:morgan.kpassi@gmail.com" class="hover:text-white">morgan.kpassi@gmail.com</a>
+          <div>(+33) 6 66 84 23 88</div>
+          <div class="flex lg:justify-start justify-center gap-3">
+              <a href="https://codepen.io/kps-243" target="_blank" rel="noopener noreferrer" aria-label="codepen">
+                <img src="/img/codepen.svg" alt="Icon CodePen" class="h-6">
+              </a>
+              <a href="https://www.linkedin.com/in/morgan-kpassi-855509211/" target="_blank" rel="noopener noreferrer"
+                aria-label="linkedin">
+                <img src="/img/linkedin.svg" alt="Icon Linkedin" class="h-6">                
+              </a>
+              <a href="https://github.com/kps-243" target="_blank" rel="noopener noreferrer" aria-label="github">
+                <img src="/img/github.svg" alt="Icon Github" class="h-6">                
+              </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
